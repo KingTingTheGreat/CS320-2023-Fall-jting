ef4cf1199849b5c259208db2cb3f4a95a580dd68
KingTingTheGreat <jting@bu.edu>
2023-09-26 15:45:55 -0400

    created tail-recurisve form of mylist_length

--- assigns/assign2/MySolution/OCaml/assign2-1.ml
+++ assigns/assign2/MySolution/OCaml/assign2-1.ml
@@ -12,3 +12,18 @@ let rec mylist_length(xs:'a mylist): int =
   (* sum of length of front and back *)
   |MyAppend2(xs1, xs2) -> mylist_length(xs1) + mylist_length(xs2)
 
+
+(* tail recursive implementation of mylist_length *)
+let mylist_length_tail(xs: 'a mylist): int = 
+  let rec tail_rec(xs:'a mylist)(acc:int): int = 
+    match xs with
+    (* an empty list has no length *)
+    |MyNil -> acc
+    (* add to accumulator and recurse *)
+    |MyCons(x1, xs) -> tail_rec xs (acc+1)
+    |MySnoc(xs, x1) -> tail_rec xs (acc+1)
+    (* reversed list has same length *)
+    |MyReverse(xs) -> tail_rec xs acc
+    (* only add accumulator to one of the recurisve calls *)
+    |MyAppend2(xs1, xs2) -> (tail_rec xs1 acc) + (tail_rec xs2 0)
+  in tail_rec xs 0
