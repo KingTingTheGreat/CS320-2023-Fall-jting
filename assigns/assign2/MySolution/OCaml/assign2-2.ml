46194e1d9eb13179a5ed86b6d5443804edc7b7cb
KingTingTheGreat <jting@bu.edu>
2023-09-26 15:15:25 -0400

    added comments to mylist_length

--- assigns/assign2/MySolution/OCaml/assign2-2.ml
+++ assigns/assign2/MySolution/OCaml/assign2-2.ml
@@ -2,10 +2,14 @@
 
 let rec mylist_length(xs:'a mylist): int = 
   match xs with
+  (* empty list has no length *)
   |MyNil -> 0
+  (* take away the single unit and recurse on the rest *)
   |MyCons(x1, xs) -> 1 + mylist_length(xs)
   |MySnoc(xs, x1) -> mylist_length(xs) + 1
+  (* reversed list has the same length *)
   |MyReverse(xs) -> mylist_length(xs)
+  (* sum of length of front and back *)
   |MyAppend2(xs1, xs2) -> mylist_length(xs1) + mylist_length(xs2)
 
 let rec mylist_get_at(xs:'a mylist)(i0:int): 'a = 
