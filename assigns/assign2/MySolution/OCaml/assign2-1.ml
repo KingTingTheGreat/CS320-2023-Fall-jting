6da03fb7e3b5e5df022a7570c1739ae16b5f8e8c
KingTingTheGreat <jting@bu.edu>
2023-09-25 23:38:06 -0400

    finished problem 1. (i think. unable to manually test with utop b/c import file not found. looks like it works fine with test file)

---
+++ assigns/assign2/MySolution/OCaml/assign2-1.ml
@@ -0,0 +1,9 @@
+#use "./../../assign2.ml";;
+
+let rec mylist_length(xs:'a mylist): int = 
+  match xs with
+  |MyNil -> 0
+  |MyCons(x1, xs) -> 1 + mylist_length(xs)
+  |MySnoc(xs, x1) -> mylist_length(xs) + 1
+  |MyReverse(xs) -> mylist_length(xs)
+  |MyAppend2(xs1, xs2) -> mylist_length(xs1) + mylist_length(xs2)
