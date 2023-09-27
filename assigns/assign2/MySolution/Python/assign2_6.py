52a66dede5bf430a3ddc1d9ae2287dca69271ddf
KingTingTheGreat <jting@bu.edu>
2023-09-27 00:36:15 -0400

    added comments to problem 6

--- assigns/assign2/MySolution/Python/assign2_6.py
+++ assigns/assign2/MySolution/Python/assign2_6.py
@@ -4,19 +4,25 @@ from MyPython import *
 from assign2_5 import *
 
 def string_length(cs:str) -> int:
+    """ returns the length of input string """
     return len(cs)   
 
 def string_get_at(cs:str, i:int) -> str:
+    assert (i < len(cs)) if i >= 0 else ((-1*i) <= len(cs))
     return cs[i]
 
 def string_make_fwork(fwork) -> str:
+    """ returns a string made from characters processed by fwork """
     xs:fnlist = fnlist_make_fwork(fwork)
     return ''.join(xs)
 
 def string_merge(cs1:str, cs2:str) -> str:
+    """ returns the merged result of two sorted strings in ascending order """
+    # initialize variables to the lengths of each string
     n1:int = string_length(cs1)
     n2:int = string_length(cs2)
     def foreach(i1:int=0, i2:int=0, work=lambda c: c) -> None:
+        """ processes each character of the output string """
         if i1 < n1:
             if i2 < n2:
                 c1, c2 = string_get_at(cs1, i1), string_get_at(cs2, i2)
