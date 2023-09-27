b7060769485cf9837ae625e37039e3af0b507ee5
KingTingTheGreat <jting@bu.edu>
2023-09-27 19:23:32 -0400

    finished problem 4

--- assigns/assign2/MySolution/OCaml/assign2-4.ml
+++ assigns/assign2/MySolution/OCaml/assign2-4.ml
@@ -1,3 +1,11 @@
 #use "./../../assign2.ml";;
+#use "./../../../../classlib/OCaml/MyOCaml.ml";;
 
 let string_sepjoin_list(sep:string)(xs:string list): string = 
+  let fopr output x = 
+    (* first string in list, don't want sep at the beginning of output *)
+    if output = "" then x
+    (* not the first string, add separator between previous and current*)
+    else string_append(output)(string_append(sep)(x))
+  in   
+  list_foldleft xs "" fopr
