a0aa8d97565f774915ad6ce916dab5556435521f
KingTingTheGreat <jting@bu.edu>
2023-11-11 19:55:02 -0500

    finished first one

--- assigns/assign6/MySolution/OCaml/assign6-1.ml
+++ assigns/assign6/MySolution/OCaml/assign6-1.ml
@@ -1,26 +1,26 @@
 #use "./../../assign6.ml";;
 #use "./../../../../classlib/OCaml/MyOCaml.ml";;
 
-let sint_to_string(x0: int): string = 
-  string_append " " (str (char_of_digit x0))
+(* coverts an int to it's corresponding string *)
+let int_to_string(x0: int): string = 
+  str (char_of_digit x0)
 ;;
 
-let rec sexpr_list_to_string(xs: sexpr list): string = 
-  match xs with
-  |[] -> ""
-  |x0 :: xs ->
-    match x0 with
-    |SInt i -> string_append (sint_to_string i) (sexpr_list_to_string xs) 
-    |_ -> ""
+(* takes in a sexpr and returns a string representation *)
+let rec sexpr_to_string(x0: sexpr): string = 
+  match x0 with 
+  |SAdd exprs -> string_append "(add" (string_append (sexpr_parse_list exprs) ")")
+  |SMul exprs -> string_append "(mul" (string_append (sexpr_parse_list exprs) ")")
+  |SInt i -> int_to_string i
 
-let sexpr_op_to_string(x0: sexpr): string = 
-  match x0 with
-  |SAdd exprs -> string_append "(add" (string_append (sexpr_list_to_string exprs) ")")
-  |SMul exprs -> string_append "(mul" (string_append (sexpr_list_to_string exprs) ")")
-  |_ -> ""
+  and 
 
-let sexpr_to_string(e: sexpr): string = 
-  sexpr_op_to_string(e)
+  (* takes in a list of sexprs and returns a string representation *)
+  sexpr_parse_list(xs: sexpr list): string = 
+    (* items in list are always preceeded by a space *)
+    match xs with
+    |[] -> ""
+    |x0::xs -> string_append " " (string_append (sexpr_parse_expr x0) (sexpr_parse_list xs))
 ;;
 
 let sexpr_parse(s: string): sexpr option = Some(SAdd [SInt 1; SInt 2]);;
